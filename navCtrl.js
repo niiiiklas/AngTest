e6ed3b85a46311acaedff4c2810c2a6d0ed56f04
Niklas Andersson <niklasandersson@Niklass-MacBook-Air.local>
2016-08-17 22:35:09 +0200

    Snippet page stuff

---
+++ navCtrl.js
@@ -0,0 +1,40 @@
+app.controller('navCtrl', ['$scope', '$interval',
+function($scope, $interval){
+
+    //initial setup
+
+    $scope.navoptions = [
+        {
+            title : "Home",
+            ref : "/views/home/home.html",
+            active : true            
+        },
+        {
+            title : "Snippets",
+            ref : "/views/main/main.html",
+            active : false 
+        },
+        {
+            title : "History",
+            ref : "/views/history/history.html",
+            active : false
+        }
+    ]
+
+    $scope.currentpageurl = $scope.navoptions[0].ref;
+    $scope.setpage = function(url){
+        $scope.currentpageurl = url;
+    }
+
+    $scope.OnNavItemClick = function (item) {
+        var title = item.title;
+
+        $scope.navoptions.forEach( function(entry){
+            entry.active = false;
+        });
+        item.active = true;
+        
+        $scope.setpage(item.ref);
+    }
+}]);
+    
